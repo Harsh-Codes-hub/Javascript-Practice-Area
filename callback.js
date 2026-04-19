7ef9f676096962ff69aa009afa851f60d1696a96
Harsh Gajrani <harshgajrani23@gmail.com>
2026-04-20 01:05:10 +0530

    Add exercises for user and product management with callback functions

--- callback.js
+++ callback.js
@@ -30,7 +30,7 @@
 // function delayedDecision(time, cb) {
 //     console.log("Processing...");
 //     setTimeout(() => {
-//         let output = time > 2000 ? "Too long" : "Quick enough"; 
+//         let output = time > 2000 ? "Too long" : "Quick enough";
 //         cb(output);
 //     }, time);
 // }
@@ -38,3 +38,114 @@
 // delayedDecision(1000, function(output) {
 //     console.log(output);
 // })
+
+// Exercise 4
+
+// function getUser(username, cb) {
+//   console.log(`Fetching data...`);
+//   setTimeout(() => {
+//     cb({
+//       id: 3098,
+//       username: "frag",
+//     });
+//   }, 1000);
+// }
+
+// function getProfile(userID, cb) {
+//   console.log(`Fetching User Data...`);
+//   setTimeout(() => {
+//     cb({
+//       bio: "It's Frag aka Harsh",
+//       followers: 70,
+//     });
+//   }, 1000);
+// }
+
+// getUser("frag", function (userData) {
+//   getProfile(userData.id, function (userProfile) {
+//     console.log(
+//       `Username: ${userData.username}\nBio: ${userProfile.bio}\nFollowers: ${userProfile.followers}`,
+//     );
+//   });
+// });
+
+// Exercise 5
+
+// function getProduct(productName, cb) {
+//   console.log("Fetching Product Data...");
+//   setTimeout(() => {
+//     cb({
+//       id: 3098,
+//       name: "laptop",
+//     });
+//   }, 1000);
+// }
+
+// function getReviews(productId, cb) {
+//   console.log("Fetching Product Review...");
+//   setTimeout(() => {
+//     cb(["good", "fine", "cheap and good"]);
+//   }, 1000);
+// }
+
+// getProduct("Laptop", function (productData) {
+//   getReviews(productData.id, function (reviews) {
+//     console.log(`Reviews: ${reviews}`);
+//   });
+// });
+
+// Exercise 6
+
+// function getUser(userName, cb) {
+//   const names = ["Frag", "Aman", "Riya", "Karan", "Neha"];
+//   console.log("Fetching User...");
+//   setTimeout(() => {
+//     const found = names.filter(
+//       (name) => name.toLowerCase() === userName.toLowerCase(),
+//     );
+//     let result = found.length > 0 ? "User Found" : "User not Found";
+//     cb(result);
+//   }, 10);
+// }
+
+// getUser("FRAG", function (result) {
+//   console.log(result);
+// });
+
+// Exercise 7
+
+// function placeOrder(product, cb) {
+//   console.log("Fetching Product Details...");
+//   setTimeout(() => {
+//     const order = {
+//       productName: product,
+//       productID: Math.floor(Math.random() * 5000),
+//       price: Math.floor(Math.random() * 50000),
+//     };
+//     cb(order);
+//   }, 1000);
+// }
+
+// function processPayment(order, cb) {
+//   console.log("Processing Payment...");
+//   setTimeout(() => {
+//     order.paymentMethod = "upi";
+//     ((order.success = true), cb(order));
+//   }, 1000);
+// }
+
+// function generateInvoice(order, cb) {
+//   console.log("Generating Invoice...");
+//   setTimeout(() => {
+//     console.log("Order Details");
+//     cb(order);
+//   }, 1000);
+// }
+
+// placeOrder("laptop", function (product) {
+//   processPayment(product, function (payment) {
+//     generateInvoice(payment, function (invoice) {
+//       console.dir(invoice);
+//     });
+//   });
+// });
