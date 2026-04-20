66393f699628bcf3a7ed4e643098b08f74100f40
Harsh Gajrani <harshgajrani23@gmail.com>
2026-04-20 17:16:43 +0530

    Completed Exercise questions

--- callback.js
+++ callback.js
@@ -149,3 +149,166 @@
 //     });
 //   });
 // });
+
+// Exercise 8
+
+// function readFile(file, cb) {
+//   console.log("Reading file...");
+//   setTimeout(() => {
+//     const document = {
+//       fileName: file,
+//       fileCreatedBY: "User",
+//       fileCreatedOn: new Date(),
+//       fileStatus: "notUpdated",
+//       fileData: {
+//         name: "frag",
+//         age: 22,
+//         gender: "male",
+//       },
+//     };
+//     cb(document);
+//   }, 1000);
+// }
+
+// function processFile(document, city, cb) {
+//   console.log("Processing file...");
+//   setTimeout(() => {
+//     document.fileData.city = city;
+//     document.fileStatus = "Updated";
+//     cb(document);
+//   }, 1000);
+// }
+
+// function saveFile(document, cb) {
+//   console.log("Saving file...");
+//   setTimeout(() => {
+//     document.isSaved = true;
+//     const saved = JSON.stringify(document);
+//     localStorage.setItem("file", saved)
+//     cb(saved);
+//   });
+// }
+
+// readFile("myIntro", function (fileData) {
+//     processFile(fileData, "Chittorgarh", function (updatedFile) {
+//         saveFile(updatedFile, function (savedFile) {
+//             console.log(savedFile);
+//         })
+//     })
+// })
+
+// Exercise 9
+
+// function getUser(userName, cb) {
+//   console.log("Fetching User...");
+//   setTimeout(() => {
+//     let found = Math.random() > 0.3 ? true : false;
+//     cb(found);
+//   }, 1000);
+// }
+
+// function getSettings(username, cb) {
+//   console.log("Fetching User Settings...");
+//   setTimeout(() => {
+//     let found = Math.random() > 0.3 ? true : false;
+//     cb(found);
+//   }, 1000);
+// }
+
+// function printResults(userFound, userSettingsFound) {
+//   console.log("Validating...");
+//   setTimeout(() => {
+//     const result = userFound && settingsFound ? "USER FOUND" : "USER NOT FOUND";
+//     console.log(result);
+//   }, 1000);
+// }
+
+// let userFound = undefined;
+// let settingsFound = undefined;
+
+// function check(param) {
+//   if (userFound !== undefined && settingsFound !== undefined)
+//     printResults(userFound, settingsFound);
+// }
+
+// getUser("frag", function (result) {
+//   userFound = result;
+//   check();
+// });
+
+// getSettings("frag", function (result) {
+//   settingsFound = result;
+//   check();
+// });
+
+// Exercise 10
+
+// function loginUser(userName, cb) {
+//   console.log("Logging in...");
+//   setTimeout(() => {
+//     const userDetails = {
+//       userName: userName,
+//       isLoggedIn: true,
+//     };
+//     console.log("User Logged In...");
+//     cb(userDetails);
+//   }, 1000);
+// }
+
+// function getUserDetails(userDetails, cb) {
+//   console.log("Fetching User Details...");
+//   setTimeout(() => {
+//     userDetails.age = 22;
+//     userDetails.gender = "Male";
+//     cb(userDetails);
+//   }, 1000);
+// }
+
+// function getUserPosts(userDetails, cb) {
+//   console.log("Fetching User Posts...");
+//   setTimeout(() => {
+//     userDetails.allPosts = {
+//       post1: {
+//         postId: 1,
+//         postName: "HEY",
+//         comments: ["HEY MAN", "YO BUDDY", "HELLO BUD"],
+//       },
+//       post2: {
+//         postId: 2,
+//         postName: "Its me Again",
+//         comments: ["GOOD TO SEE YOU AGAIN", "HELLO MATE"],
+//       },
+//       post3: {
+//         postId: 3,
+//         postName: "I AM BACK",
+//         comments: ["YO MAN WHATS UP", "GOOD TO SEE YOU MATE"],
+//       },
+//     };
+//     cb(userDetails);
+//   }, 1000);
+// }
+
+// function getCommentsForPost(userDetails, postNumber, cb) {
+//   console.log(`Fetching Post No. ${postNumber} Comment...`);
+//   setTimeout(() => {
+//     let postToSee = postNumber;
+//     let allPosts = JSON.parse(JSON.stringify(userDetails.allPosts));
+//     for (const key in allPosts) {
+//       const element = allPosts[key];
+//       if (element.postId === postNumber) {
+//         cb(element.comments);
+//         return;
+//       }
+//     }
+//   }, 1000);
+// }
+
+// loginUser("Frag", function (userDetails) {
+//   getUserDetails(userDetails, function (userDetails) {
+//     getUserPosts(userDetails, function (userDetails) {
+//       getCommentsForPost(userDetails, 3, function (comments) {
+//         console.log(comments);
+//       });
+//     });
+//   });
+// });
